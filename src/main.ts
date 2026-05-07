2b970b90e97472ea1fd77458894a2643581107fc
VittorioDauria94 <dauriavittorio94@gmail.com>
2026-05-07 09:28:54 +0200

    feat: milestone 4 done

--- src/main.ts
+++ src/main.ts
@@ -53,7 +53,7 @@ async function getActress(id: number): Promise<Actress | null> {
       throw new Error("Errore nella risposta del server");
     }
 
-    const data = await resp.json();
+    const data: unknown = await resp.json();
 
     if (!isActress(data)) {
       throw new Error("Formato dati non valido");
@@ -64,4 +64,29 @@ async function getActress(id: number): Promise<Actress | null> {
     console.error(error);
     return null;
   }
+}
+
+async function getAllActresses(): Promise<Actress[]> {
+  const resp = await fetch("http://localhost:3333/actresses");
+
+  if (!resp.ok) {
+    throw new Error("Errore nella risposta del server");
+  }
+
+  const data: unknown = await resp.json();
+
+  if (!Array.isArray(data)) {
+    throw new Error("Formato dati non valido");
+  }
+
+  const actresses: Actress[] = [];
+
+  data.forEach((actress: unknown) => {
+    if (!isActress(actress)) {
+      throw new Error("Formato dati non valido");
+    }
+    actresses.push(actress);
+  });
+
+  return actresses;
 }
